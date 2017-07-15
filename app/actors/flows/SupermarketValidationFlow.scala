90201ad2bbbf6ff51308a7ad1190db155ce10c1a
Clay Teeter <clay.teeter@maalka.com>
2017-07-14 18:51:25 -0600

    Fixed type in SupermarketValidationFlow

--- app/actors/flows/SupermarketValidationFlow.scala
+++ app/actors/flows/SupermarketValidationFlow.scala
@@ -16,8 +16,9 @@ import play.api.libs.json.Json
 case class SupermarketValidationFlow @Inject()(validateFlow: ValidateFlow) {
   val fields = Seq[(String, Double, Double)](
     ("Site Energy Resource Intensity", 328, 828),
-    ("Food Sales-Grocery Store Gross Area", 19785, 92961)),
+    ("Food Sales-Grocery Store Gross Area", 19785, 92961),
     ("Food Sales-Grocery Store Business Average Weekly Hours", 98, 164)
+  )
 
   val densityFields = Seq[(String, Double, Double)](
     ("Food Sales-Grocery Store Workers on Main Shift Quantity", 0.4, 1.5),
