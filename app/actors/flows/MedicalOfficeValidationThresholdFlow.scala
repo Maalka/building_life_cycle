ff4387d64eb39028bc9227048acbf84b4e584265
Clay Teeter <clay.teeter@maalka.com>
2017-07-26 20:15:44 -0700

    Added Copyright

--- app/actors/flows/MedicalOfficeValidationThresholdFlow.scala
+++ app/actors/flows/MedicalOfficeValidationThresholdFlow.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Maalka
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package actors.flows
 
 import javax.inject.Inject
@@ -9,11 +25,6 @@ import akka.stream.scaladsl._
 import com.maalka.bedes.BEDESTransformResult
 import play.api.Configuration
 import play.api.libs.json.Json
-
-/**
-  * Created by clayteeter on 7/14/17.
-  */
-
 
 class MedicalOfficeValidationThresholdFlow @Inject ()( override implicit val actorSystem: ActorSystem,
                                                             override implicit val configuration: Configuration)
--- app/actors/validators/basic/Contains.scala
+++ app/actors/validators/basic/Contains.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Maalka
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package actors.validators.basic
 
 import java.util.UUID
@@ -9,12 +25,6 @@ import actors.validators.Validator.MapValid
 
 
 import scala.concurrent.Future
-
-/**
-  * Validates that MaterData includes 'stringValue' that is equal to a String passed in arguments
-  *
-  * Created by tadassugintas on 2016-12-13.
-  */
 
 object Contains {
 
