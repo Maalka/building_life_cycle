d51f514704a4b45798713ffe442dc7a669df6ee1
Melissa Teeter <melissa.hendricks@maalka.com>
2018-03-01 14:32:09 -0700

    Rearranged the layout to put timeline graph on the bottom. Put 1px width column under flags to emulate line, and used set value for y to create different heights for labels.

--- app/assets/javascripts/common/directives/timeline.js
+++ app/assets/javascripts/common/directives/timeline.js
@@ -25,12 +25,12 @@ define(['angular', 'moment', 'highcharts', 'highcharts-drilldown', 'highcharts-b
                         backgroundColor: "transparent",
                         style: {
                             fontFamily: 'Gesta',
-                        }
+                        },
+                        margin: 80
                     },
-                    yAxis: {
+                    xAxis: {
 						title: {
 							text: 'Year',
-							margin: 20
 						},
                         type: "datetime",
                         tickInterval: 24 * 3600 * 1000 * 365,
@@ -40,7 +40,7 @@ define(['angular', 'moment', 'highcharts', 'highcharts-drilldown', 'highcharts-b
 						lineColor: 'transparent'
                     },
 
-                    xAxis: {
+                    yAxis: {
 						max: 100,
 						min: 1,
 						visible: false
@@ -59,27 +59,42 @@ define(['angular', 'moment', 'highcharts', 'highcharts-drilldown', 'highcharts-b
 							style: { // text style
 								color: 'white',
 								fontSize: 12,
-							}
+							},
 						}
 					},
 
 					series: [{
+                        type: 'column',
+                        name: 'Observations',
+                        id: 'dataseries',
+                        pointWidth: 2,
+                        data: [ { x: Date.UTC(2000, 5, 26), y: (0), color: '#06A1F9'},
+                                { x: Date.UTC(2005, 4, 20), y: (40), color: '#0D95BB'},
+                                { x: Date.UTC(2006, 3, 14), y: (80), color: '#0A708C'},
+                                { x: Date.UTC(2011, 5, 17), y: (40), color: '#2F4598'},
+                                { x: Date.UTC(2012, 11, 23), y: (0), color: '#5D70D4'}
+                                ],
+                        marker: {
+                            radius: 4
+                        }
+                    },{
                         type: 'flags',
-                        shape: 'flag',
-                        borderRadius: 2,
+                        allowOverlapX: false,
+                        shape: 'squarepin',
                         useHTML: true,
+                        onSeries: 'dataseries',
 						data: [
-							{ y: Date.UTC(2000, 5, 26), x: (10), text: 'Comment and date', title: '<span style="margin: 15px">Duct Installation </span>', color: '#06A1F9', fillColor: '#06A1F9'},
-							{ y: Date.UTC(2005, 4, 20), x: (60), text: 'Comment and date', title: '<span style="padding: 15px">Hot Water Heating</span>', color: '#0D95BB', fillColor: '#0D95BB'},
-							{ y: Date.UTC(2006, 3, 14), x: (20), text: 'Comment and date', title: '<span style="padding: 15px">Glass Exterior Door</span>', color: '#0A708C', fillColor: '#0A708C'},
-							{ y: Date.UTC(2011, 5, 17), x: (60), text: 'Comment and date', title: '<span style="padding: 15px">Wood Ceiling Finish</span>', color: '#2F4598', fillColor: '#2F4598'},
-							{ y: Date.UTC(2012, 11, 23), x: (20), text: 'Comment and date', title: '<span style="padding: 15px">Stepped Dimming Lighting Control</span>', color: '#5D70D4', fillColor: '#5D70D4'},
+							{ x: Date.UTC(2000, 5, 26), text: 'Comment and date', title: '<span style="margin: 15px;">Duct Installation </span>', color: '#06A1F9', fillColor: '#06A1F9'},
+							{ x: Date.UTC(2005, 4, 20), text: 'Comment and date', title: '<span style="margin: 15px">Hot Water Heating</span>', color: '#0D95BB', fillColor: '#0D95BB'},
+							{ x: Date.UTC(2006, 3, 14), text: 'Comment and date', title: '<span style="margin: 15px">Glass Exterior Door</span>', color: '#0A708C', fillColor: '#0A708C'},
+							{ x: Date.UTC(2011, 5, 17), text: 'Comment and date', title: '<span style="margin: 15px">Wood Ceiling Finish</span>', color: '#2F4598', fillColor: '#2F4598'},
+							{ x: Date.UTC(2012, 11, 23), text: 'Comment and date', title: '<span style="margin: 15px">Stepped Dimming Lighting Control</span>', color: '#5D70D4', fillColor: '#5D70D4'},
 							],
                         },
                     ]
                 };
                  $timeout(function () { 
-                    angular.element($element).height(500).highcharts(options);
+                    angular.element($element).height(300).highcharts(options);
                 }, 0);                
             }]
         };
